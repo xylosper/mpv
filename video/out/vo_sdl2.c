e5d4dfcb2e8a2090408b562337107b2c8a6a0b3f
Rudolf Polzer <divverent@xonotic.org>
2012-12-09 20:55:48 +0100

    sdl2: now compiles, and tries to create window, but segfaults in SDL_CreateWindow

--- video/out/vo_sdl2.c
+++ video/out/vo_sdl2.c
@@ -47,13 +47,13 @@
 #include "core/mp_fifo.h"
 
 struct priv {
-	SDL_Window *window;
-	SDL_Renderer *renderer;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
 };
 
 static int config(struct vo *vo, uint32_t width, uint32_t height,
-                  uint32_t d_width, uint32_t d_height, uint32_t flags,
-                  uint32_t format)
+        uint32_t d_width, uint32_t d_height, uint32_t flags,
+        uint32_t format)
 {
     struct priv *vc = vo->priv;
     if (vc->renderer) {
@@ -64,7 +64,12 @@ static int config(struct vo *vo, uint32_t width, uint32_t height,
         SDL_DestroyWindow(vc->window);
         vc->window = NULL;
     }
-    vc->window = SDL_CreateWindow("MPV", 0, 0, d_width, d_height, 0);
+    Uint32 winflags = SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE;
+    if (vo_fs)
+        winflags |= SDL_WINDOW_FULLSCREEN;
+    vc->window = SDL_CreateWindow("MPV",
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, d_width, d_height,
+            winflags);
     if (!vc->window) {
         mp_msg(MSGT_VO, MSGL_ERR, "Could not get a SDL2 window");
         return -1;
@@ -76,16 +81,6 @@ static int config(struct vo *vo, uint32_t width, uint32_t height,
         vc->window = NULL;
         return -1;
     }
-}
-
-static uint32_t draw_image(struct vo *vo, mp_image_t *mpi)
-{
-    return true;
-}
-
-static int draw_slice(struct vo *vo, uint8_t *src[], int stride[], int w, int h,
-                      int x, int y)
-{
     return 0;
 }
 
@@ -99,7 +94,10 @@ static void check_events(struct vo *vo)
 
 static void uninit(struct vo *vo)
 {
+    struct priv *vc = vo->priv;
     SDL_QuitSubSystem(SDL_INIT_VIDEO);
+    talloc_free(vc);
+    vo->priv = NULL;
 }
 
 static void draw_osd(struct vo *vo, struct osd_state *osd)
@@ -108,18 +106,18 @@ static void draw_osd(struct vo *vo, struct osd_state *osd)
 
 static int preinit(struct vo *vo, const char *arg)
 {
+    struct priv *vc;
+    vo->priv = talloc_zero(vo, struct priv);
+    vc = vo->priv;
     if(SDL_WasInit(SDL_INIT_VIDEO)) {
         mp_msg(MSGT_VO, MSGL_ERR, "SDL2 already initialized");
         return -1;
     }
-    SDL_InitSubSystem(SDL_INIT_VIDEO);
-    struct priv *vc;
-    vo->priv = talloc_zero(vo, struct priv);
-    vc = vo->priv;
+    SDL_Init(SDL_INIT_VIDEO);
     return 0;
 }
 
-static int query_format(uint32_t format)
+static int query_format(struct vo *vo, uint32_t format)
 {
     return VFCAP_CSP_SUPPORTED; // TODO
 }
@@ -128,8 +126,8 @@ static int control(struct vo *vo, uint32_t request, void *data)
 {
     struct priv *vc = vo->priv;
     switch (request) {
-    case VOCTRL_QUERY_FORMAT:
-        return query_format(vo, *((uint32_t *)data));
+        case VOCTRL_QUERY_FORMAT:
+            return query_format(vo, *((uint32_t *)data));
     }
     return VO_NOTIMPL;
 }
